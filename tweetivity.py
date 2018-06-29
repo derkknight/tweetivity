2c9326f81a3beb4959c07d12045b277d92d23235
niseguy <dmy4874@rit.edu>
2018-06-28 20:28:15 -0400

    Logging addition

--- tweetivity.py
+++ tweetivity.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template
+from flask.logging import default_handler
 import TweetMunger as tm
+import logging
+from logging.handlers import RotatingFileHandler
+from logging.config import dictConfig
+import datetime
+
 app = Flask(__name__)
- 
+
+#Error handling
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+    return render_template('index.html', error = "An error has occurred. Please try again."), 500
+
+
 @app.route("/")
 def index():
+    app.logger.info('LANDED')
     return render_template('index.html')
 
 @app.route("/<string:name>/<string:offset>")
@@ -13,4 +26,7 @@ def getFollowers(name, offset):
     return render_template('graph.html', data = data, name = name)
 
 if __name__ == "__main__":
+    handler = RotatingFileHandler('stats.log', backupCount=1)
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
     app.run()
